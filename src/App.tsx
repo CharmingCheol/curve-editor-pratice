23139d872a97581c92d58247a4a0d792f39520d4
CharmingCheol <toby0806@naver.com>
2021-06-24 16:56:09 +0900

    리덕스 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,8 @@
-import React, { useCallback, useState, Fragment } from "react";
-import One from "./history/0617/2";
-import Frozen from "./history/0617/3";
+import React from "react";
 import Container from "./Container";
 
-type Selected = "1" | "frozen" | "now";
-
 const App = () => {
-  const [menu, setMenu] = useState<Selected>("1");
-
-  const handleClickButton = useCallback((selected: Selected) => {
-    setMenu(selected);
-  }, []);
-
-  const Rendered = () => {
-    if (menu === "1") {
-      return <One />;
-    } else if (menu === "frozen") {
-      return <Frozen />;
-    }
-    return <Container />;
-  };
-
-  return (
-    <Fragment>
-      <button onClick={() => handleClickButton("1")}>라인 1개</button>
-      <button onClick={() => handleClickButton("frozen")}>굉장히 버벅임</button>
-      <button onClick={() => handleClickButton("now")}>현재</button>
-      <Rendered />
-    </Fragment>
-  );
+  return <Container />;
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import App from "./App";
+import rootReducer from "./reducers";
 import "./index.css";
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...[]))
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
